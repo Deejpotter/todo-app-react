5a1d1ee3df7223c17fa533467ab7e0a8af66f181
Deej <deejpotter@gmail.com>
2021-08-12 18:38:21 +1000

    Started setting up the modal.

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from '../Util/Container';
+
+function Modal(props) {
+    return (
+        <Container ext='modal'>
+            
+        </Container>
+    );
+}
+
+export default Modal;
